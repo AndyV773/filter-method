0ceb15618aae8e636ac25b279d130b3924a66e61
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2024-11-05 11:03:55 +0000

    filter method

--- filter.js
+++ filter.js
@@ -1,4 +1,4 @@
-  
+
 /**
  * To run this file in Gitpod, use the 
  * command node filter.js in the terminal
@@ -20,6 +20,12 @@ const people = [
     age: 18,
   },
 ];
+
+const oldEnough = people.filter(person => person.age >= 21);
+console.log(oldEnough);
+
+const paul = people.filter(p => p.name === 'Paul')[0];
+console.log(paul)
 
 
 // Complex Filtering
@@ -55,3 +61,25 @@ const students = [
     ]
   },
 ];
+
+// const candidates = students.filter(student => {
+//   let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5);
+//   return strongSkills.length > 0;
+// });
+// console.log(candidates);
+
+// const hasStrongSkills = student => {
+//   let strongSkills = student.skills.filter(skill => skill.yrsExperience >= 5);
+//   return strongSkills.length > 0;
+// };
+
+// const candidates = students.filter(hasStrongSkills);
+// console.log(candidates);
+
+const has5yearsExp = skill => skill.yrsExperience >= 5;
+const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;
+const candidates = students.filter(hasStrongSkills);
+console.log(candidates);
+
+const strongNames = candidates.map(n => n.name);
+console.log(strongNames);
